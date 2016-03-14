ea185c72f7335b711f5f3376dbaf8b5d72b407c6
Andreas Eger <dev@eger-andreas.de>
2016-03-14 22:28:44 +0100

    fix wrapper

--- receptacle.rb
+++ receptacle.rb
@@ -36,50 +36,50 @@ module Receptacle
             raise "not configured"
           end
 
-          wrappers(self, method_name, args) do |call_args|
+          with_wrappers(self, method_name, args) do |call_args|
             strategy.new.public_send(method_name, call_args)
           end
         end
       end
-      def wrappers(base, method_name, args, &block)
+      
+      def with_wrappers(base, method_name, args, &block)
         wrappers = Registration.instance.wrappers[base]
-        if wrappers.empty?
-          block.call(args) 
-        else
-          wrappers = wrappers.map(&:new)
-          call_args = wrappers.reduce(args) do |memo, wrapper|
-            wrapper.public_send("before_#{method_name}", memo)
-          end
-          ret = wrappers.reduce do |memo, wrapper|
-            wrapper.public_send("around_#{method_name}", call_args, memo, &block)
-          end
-          wrappers.reverse.reduce(ret) do |memo, wrapper|
-            wrapper.public_send("after_#{method_name}", call_args, memo)
-          end
+        return block.call(args) if wrappers.empty?
+
+        wrappers = wrappers.map(&:new)
+        args = before_wrapper(wrappers, method_name, args)
+        ret = around_wrapper(wrappers, method_name, args, &block)
+        after_wrapper(wrappers, method_name, args, ret)
+      end
+
+      def before_wrapper(wrappers, method_name, args)
+        before_method_name = "before_#{method_name}"
+        before_wrapper = wrappers.select{|e| e.respond_to?(before_method_name)}
+        return args if before_wrapper.empty?
+
+        before_wrapper.reduce(args) do |memo, wrapper|
+          wrapper.public_send(before_method_name, memo)
         end
       end
-    end
-  end
-  module Wrapper
-    def self.included(base)
-      base.extend(ClassMethods)
-    end
-    def method_missing(method_sym, *args, &block)
-      method_name = method_sym.to_s
-      unless %w(before after around).any?{|e| method_name.start_with?(e) }
-        super
-      end
-    end
 
-    module ClassMethods
-      def before(method_name, &block)
-        define_method("before_#{method_name}", &block)
+      def around_wrapper(wrappers, method_name, args, &block)
+        around_method_name = "around_#{method_name}"
+        around_wrapper = wrappers.select{|e| e.respond_to?(around_method_name)}
+        return block.call(args) if around_wrapper.empty?
+
+        around_wrapper.reduce(nil) do |memo, wrapper|
+          wrapper.public_send(around_method_name, args, memo, &block)
+        end
       end
-      def around(method_name, &block)
-        define_method("before_#{method_name}", &block)
-      end
-      def after(method_name, &block)
-        define_method("before_#{method_name}", &block)
+
+      def after_wrapper(wrappers, method_name, args, return_value)
+        after_method_name = "after_#{method_name}"
+        after_wrapper  = wrappers.select{|e| e.respond_to?(after_method_name)}.reverse
+        return return_value if after_wrapper.empty?
+
+        after_wrapper.reduce(return_value) do |memo, wrapper|
+          wrapper.public_send(after_method_name, args, memo)
+        end
       end
     end
   end
@@ -92,44 +92,33 @@ module Repo
     module Strategy
       class Fake
         def where(args)
-          p "Fake#where #{args}"
+          puts "Fake#where #{args}"
           :where
-        end
-      end
-      class Real
-        def where(args)
-          p "Real#where #{args}"
         end
       end
     end
     module Wrappers
       class First
-        include Receptacle::Wrapper
-        before :where do |args|
+        def before_where(args)
           puts "First#before"
           args
         end
-        # after :where do |args, return_values|
-        #   puts "First#after"
-        #   return_values
-        # end
+        def after_where(args, return_values)
+          puts "First#after"
+          return_values
+        end
       end
       class Second
-        include Receptacle::Wrapper
-        # around :where do |args, ret, &block|
-        #   puts "Second#around - before"
-        #   ret = block.call(args)
-        #   puts "Second#around - after"
-        #   ret
-        # end
-        # before :where do |args|
-        #   puts "Second#before"
-        #   args
-        # end
-        # after :where do |args, return_values|
-        #   puts "Second#after"
-        #   return_values
-        # end
+        def before_where(args)
+          puts "Second#before"
+          self.state = 24
+          args
+        end
+        def after_where(args, return_values)
+          puts "Second#after with around state: #{state}"
+          return_values
+        end
+        attr_accessor :state
       end
     end
   end
