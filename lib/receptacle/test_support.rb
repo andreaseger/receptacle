4b03c0895f89f7621f3f7b895dfc28e51d346f9f
Andreas Eger <dev@eger-andreas.de>
2017-02-02 00:24:28 +0100

    update travis.yml

--- lib/receptacle/test_support.rb
+++ lib/receptacle/test_support.rb
@@ -1,43 +1,43 @@
+# frozen_string_literal: true
 module Receptacle
-  # provides helpful method to toggle strategies during testing
-  #
-  # can be used in rspec like this
-  #
-  #    require 'receptacle/test_support'y
-  #    RSpec.configure do |c|
-  #      c.include Receptacle::TestSupport
-  #    end
-  #
-  #    RSpec.describe(UserRepository) do
-  #      around do |example|
-  #        with_strategy(strategy){example.run}
-  #      end
-  #      let(:strategy) { UserRepository::Strategy::MySQL }
-  #      ...
-  #    end
-  #
-  # or similar with minitest
-  #
-  #    require 'receptacle/test_support'
-  #    class UserRepositoryTest < Minitest::Test
-  #      def described_class
-  #        UserRepository
-  #      end
-  #      def repo_exists(strategy)
-  #        with_strategy(strategy) do
-  #          assert described_class.exists(id: 5)
-  #        end
-  #      end
-  #      def test_mysql
-  #        repo_exists(UserRepository::Strategy::MySQL)
-  #      end
-  #      def test_fake
-  #        repo_exists(UserRepository::Strategy::Fake)
-  #      end
-  #    end
-
   module TestSupport
-    def with_strategy(strategy, repo = described_class, &block)
+    # provides helpful method to toggle strategies during testing
+    #
+    # can be used in rspec like this
+    #
+    #    require 'receptacle/test_support'y
+    #    RSpec.configure do |c|
+    #      c.include Receptacle::TestSupport
+    #    end
+    #
+    #    RSpec.describe(UserRepository) do
+    #      around do |example|
+    #        with_strategy(strategy){example.run}
+    #      end
+    #      let(:strategy) { UserRepository::Strategy::MySQL }
+    #      ...
+    #    end
+    #
+    # or similar with minitest
+    #
+    #    require 'receptacle/test_support'
+    #    class UserRepositoryTest < Minitest::Test
+    #      def described_class
+    #        UserRepository
+    #      end
+    #      def repo_exists(strategy)
+    #        with_strategy(strategy) do
+    #          assert described_class.exists(id: 5)
+    #        end
+    #      end
+    #      def test_mysql
+    #        repo_exists(UserRepository::Strategy::MySQL)
+    #      end
+    #      def test_fake
+    #        repo_exists(UserRepository::Strategy::Fake)
+    #      end
+    #    end
+    def with_strategy(strategy, repo = described_class)
       original_strategy = repo.strategy
       repo.strategy strategy
       yield
